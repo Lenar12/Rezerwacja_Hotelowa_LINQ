54a21317799a44f3adff6a456aed54d340299374
Lenar12 <berta1116@g.pl>
2015-06-13 06:01:28 +0200

    Małe zmiany

---
+++ Rezerwacja Hotelowa LINQ/Panel_recepcjonisty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rezerwacja_Hotelowa_LINQ
+{
+    public partial class Panel_recepcjonisty : Form
+    {
+        Baza_Danych_LINQDataContext dc = new Baza_Danych_LINQDataContext();
+        public Panel_recepcjonisty()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var data = (from c in dc.Pracownicies where c.login == "1" select new { c.login, c.haslo, c.poziom_uprawnien });
+            dataGridView1.DataSource = data;
+            
+        }
+        
+    }
+}
--- Rezerwacja Hotelowa LINQ/Program.cs
+++ Rezerwacja Hotelowa LINQ/Program.cs
@@ -16,7 +16,7 @@ namespace Rezerwacja_Hotelowa_LINQ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Panel_Logowania());
         }
     }
 }
